3e0204bc60d4bc209a51ad00d89b72b3ffb3e08d
Jzefla7u7 <43443823+Jzefla7u7@users.noreply.github.com>
2019-01-17 09:51:25 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,18 @@
+$("#taxt").hide();
+
+
+$("#hide ").click(function() {
+   $("img   ").hide();
+});
+
+
+
+$("#button").click(function() {
+  $("body").css("background-color","black");
+  $("#taxt").show();
+});
+$("#button1").click(function() {
+  $("body").css("background-color","black");
+});
+ $("#button1").click(function() {
+});
